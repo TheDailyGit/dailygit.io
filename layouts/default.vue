83fff5ea552a0adb356707a38b134fb767675abb
xamthor <miguel@mhernandez.me>
2021-04-09 16:00:44 -0500

    Fresh

---
+++ layouts/default.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="">
+    <Nuxt />
+  </div>
+</template>
+
+<style>
+html{
+	@apply bg-black
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,11 @@
+<template>
+	<div>
+		<Logo class="mx-auto"/>
+		<p class="text-white text-center">comng soon</p>
+	</div>	
+</template>
+
+<script>
+export default {}
+</script>
+
